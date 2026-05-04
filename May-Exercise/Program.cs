40b9a07ab30762262716affe8591b931fe1ee12a
Wolfram Klingspiegel <wolfram.klingspiegel@gmail.com>
2026-05-04 16:33:15 +0200

    Projektdateien hinzufügen.

---
+++ May-Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 3, 3, 4, 5, 5, 7 };
+
+        int uniqueOddCount = CountUniqueOddNumbers(numbers);
+
+        Console.WriteLine($"Unique odd numbers: {uniqueOddCount}");
+    }
+
+    public static int CountUniqueOddNumbers(int[] numbers)
+    {
+        if (numbers == null)
+        {
+            throw new ArgumentNullException(nameof(numbers), "The array should not be null.");
+        }
+
+        HashSet<int> uniqueOddNumbers = new HashSet<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0)
+            {
+                uniqueOddNumbers.Add(number);
+            }
+        }
+
+        return uniqueOddNumbers.Count;
+    }
+}
